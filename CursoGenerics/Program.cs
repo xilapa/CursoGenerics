725b6cc1e4ce577bad36c89874cf93f9d2f2cc73
dirceu junior <dirceu.sjr@gmail.com>
2021-04-28 09:49:26 -0300

    initial commit

---
+++ CursoGenerics/Program.cs
@@ -0,0 +1,19 @@
+﻿using CursoGenerics.Circular_Buffer;
+using System;
+using System.Collections.Generic;
+
+namespace CursoGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var a = new CircularBuffer<double>();
+
+            var lista = new List<string>();
+            lista.C
+
+        }
+    }
+}
