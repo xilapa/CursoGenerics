7ef75e71aa373f033a9ac5ec0697ce5f42fffaa9
dirceu junior <dirceu.sjr@gmail.com>
2021-05-01 18:10:29 -0300

    LinkedList and Dictionary testing in Program.cs

--- CursoGenerics/Program.cs
+++ CursoGenerics/Program.cs
@@ -8,12 +8,38 @@ namespace CursoGenerics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            var a = new CircularBuffer<double>();
+            #region LinkedList
+            Console.WriteLine("Linked List Example");
+            var linkedList = new LinkedList<int>();
 
-            var lista = new List<string>();
-            lista.C
+            linkedList.AddFirst(1);
+            linkedList.AddFirst(2);
 
+            var firstNode = linkedList.First;
+            linkedList.AddAfter(firstNode, 10);
+            linkedList.AddBefore(firstNode, 9);
+
+            firstNode = linkedList.First;
+            while (firstNode is not null)
+            {
+                Console.WriteLine(firstNode.Value);
+                firstNode = firstNode.Next;
+            }
+            Console.WriteLine("-----");
+            #endregion
+
+            #region Dictionary
+            Console.WriteLine("Dictionary Example");
+            var dictionary = new Dictionary<string, int>();
+            dictionary.Add("um", 1);
+            dictionary.Add("dois", 2);
+            dictionary.Add("três", 3);
+
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine(item);
+            }
+            #endregion
         }
     }
 }
